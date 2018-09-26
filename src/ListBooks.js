7d36a6caf63e8697327bae35a136175e0ecf7238
Aizhan Osmonova <osmonova86@gmail.com>
2018-09-26 12:59:15 -0400

    feat: Add router to browse pages, load Books data and pass to components

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+
 
 class ListBooks extends React.Component{
 
-  render() {      
-    return (
+  render() {
 
+  	let {books} = this.props
+  	      
+    return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
@@ -161,7 +165,7 @@ class ListBooks extends React.Component{
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
 
