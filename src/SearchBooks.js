08ec466111d123873925d1ad3d42568b56b5211f
Aizhan Osmonova <osmonova86@gmail.com>
2018-09-26 13:58:56 -0400

    feat: Search Books API and display result

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
+import sortBy from 'sort-by';
+import Book from './Book';
+import * as BooksAPI from './BooksAPI';
 
 class SearchBooks extends React.Component{
+
+  state={
+    query: '',
+    books: [],
+    searchResults: [],
+    searchError: false
+  };
 
   static propTypes = {
     books: PropTypes.array.isRequired
   }
 
+  updateQuery = (query)=>{
+      this.setState({query: query.trim()});
+  }
+
   render() {
 
-  	let {books} = this.props
+      const {books} = this.props;
+      const {query, searchResults, searchError} = this.state;
+
+      if(this.state.query){
+        BooksAPI.search(query, 20).then((foundBooks)=>{
+            if(foundBooks.length>0){
+              this.setState({searchResults: foundBooks, searchError: false});
+            }else {
+              this.setState({searchResults: [], searchError: true});
+            }
+        });
+        searchResults.sort(sortBy('title'));
+      }
 
     return (
 	/*
@@ -22,18 +48,39 @@ class SearchBooks extends React.Component{
         <div className="search-books-bar">
           <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            {}
-            <input type="text" placeholder="Search by title or author"/>
-
+            {
+                  <input type="text" 
+                    placeholder="Search by title or author"
+                    value={query}
+                    onChange={(event)=>this.updateQuery(event.target.value)}
+                  />                
+            }
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
+
+        {searchResults.length>0 && (            
+          <div className="search-books-results">
+              <div className='showing-books'>
+                  <h3>{searchResults.length} items found</h3>
+              </div>            
+                    
+              <ol className="books-grid">
+               {searchResults.map((book) => (
+                  <li key={book.id}>
+                    <Book book={book} books={books}/>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          )}
+        {searchError && (            
+          <div className="search-books-results">
+            <div className='showing-books'>
+              <h3>No results match '{query}': 0 items found</h3>
+            </div>                                 
+          </div>
+        )}
       </div>
-
-
-
     )
   }
 }
